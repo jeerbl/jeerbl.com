96f5000b2f7c957453ea8bf7579fb8fb18521c87
jeerbl <jb.jeromebrunel@gmail.com>
2019-02-12 01:43:08 -0500

    Start container with certificate

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 docker rm -f website-http
-docker run --name website-http -p 80:80 -d website:latest
+docker run --name website-http -p 80:80 -p 443:443 -v /etc/letsencrypt:/certificates -d website:latest
