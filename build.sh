cd96f0d263f4fdd09c2119c4baaf55f4681586e9
jeerbl <jb.jeromebrunel@gmail.com>
2019-02-12 00:11:41 -0500

    Update build

--- build.sh
+++ build.sh
@@ -1,10 +1,17 @@
+#!/bin/sh
+
+git clone https://github.com/jeerbl/jeerbl.com.git website
+cd website
+
 docker build -t build -f Dockerfile.build .
 docker create --name build-container build
-
-rm -rf ./build
-mkdir build
 
 docker cp build-container:/jeerbl.com/dist/. ./build
 
 docker rm -f build-container
 docker rmi build
+
+docker build -t website .
+
+cd ..
+rm -rf website
